b5ed7c287e4d3ed6c1079f98c936e964c32becbc
Robert Ramirez <metaknight757@gmail.com>
2019-10-23 18:40:14 -0500

    added react router dom

---
+++ src/components/main.js
@@ -0,0 +1,10 @@
+import React from "react";
+import LandingPage from "./landingpage";
+
+const Main = () => {
+    <switch>
+        <Route exact path="/" component={LandingPage} />
+    </switch>
+}
+
+export default Main;
