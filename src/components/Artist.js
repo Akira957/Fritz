87d81d3961f350666a9d5091f9c984bc2ae19c29
Robert Ramirez <metaknight757@gmail.com>
2019-11-12 15:21:59 -0600

    Started content in artist page

--- src/components/Artist.js
+++ src/components/Artist.js
@@ -1,6 +1,36 @@
 import React, { Component } from "react";
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
 
 class Artist extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { activeTab: 0 };
+      }
+    
+      toggleCategories() {
+    
+        if(this.state.activeTab === 0){
+          return(
+            <div className="artists-grid">
+              
+            </div>
+          )
+        } else if(this.state.activeTab === 1) {
+          return (
+            <div><h1>Artist 1</h1></div>
+          )
+        } else if(this.state.activeTab === 2) {
+          return (
+            <div><h1>Artist 2</h1></div>
+          )
+        } else if(this.state.activeTab === 3) {
+          return (
+            <div><h1>Artist 3</h1></div>
+          )
+        }
+      }
+
     render() {
         return (
             <h1>Names of the artists</h1>
